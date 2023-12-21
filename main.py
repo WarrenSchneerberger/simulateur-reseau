098cf29c7f9721dbe186cc820b650344b464727f
schneerberger <warren@butterfly-training.net>
2023-12-21 15:52:38 +0100

    ajout de la fonction calcule de distance A* & Dijkstra + visuel

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 import random
 import math
 from collections import deque
+import heapq
+
+isNotRun = True
+noeudA = "0"
+noeudB = "0"
+text_result = ""
 
 def PositionsNoeuds(n_noeuds, largeur, hauteur, rayon_noeud):
     positions_noeuds = []
@@ -23,7 +29,6 @@ def PositionsNoeuds(n_noeuds, largeur, hauteur, rayon_noeud):
                 positions_noeuds.append(position)
                 break
     return positions_noeuds
-
 
 def PositionLiens(n_liens, n_noeuds):
     liens = []
@@ -63,10 +68,223 @@ def PositionLiens(n_liens, n_noeuds):
         liens.append(lien)
     return liens
 
+def DrawLink(fenetre, noir, positions_noeuds, position_liens):
+    for position in position_liens:
+        pygame.draw.line(fenetre, noir, positions_noeuds[position[0]], positions_noeuds[position[1]], 2)
+
+def DrawNoeud(positions_noeuds,fenetre, noir, blanc, rayon_noeud):
+    for i, position in enumerate(positions_noeuds):
+        pygame.draw.circle(fenetre, noir, position, rayon_noeud)
+        # Afficher le numéro d'identification
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(i), True, blanc)
+        text_rect = text.get_rect()
+        text_rect.center = position
+        fenetre.blit(text, text_rect)
+
+def DrawDiametre(fenetre, positions_noeuds, position_liens, noir):
+    diametre = CalculerDiametre(positions_noeuds, position_liens)
+    font = pygame.font.Font(None, 36)
+    # Elements de l'interface
+    diametre_text = pygame.Rect(200, 0, 140, 32)
+    # Afficher le texte des champs de texte
+    texte_diametre = font.render("Le diamètre du réseau est :"+str(diametre), True, noir)
+    fenetre.blit(texte_diametre, (diametre_text.x + 5, diametre_text.y + 5))
+
+def DrawCalDistance(fenetre, noir, click, positions_noeuds, position_liens):
+    nb_noeud = len(positions_noeuds)
+    font_title = pygame.font.Font(None, 30)
+    font = pygame.font.Font(None, 48)
+    font_40 = pygame.font.Font(None, 40)
+    font_25 = pygame.font.Font(None, 25)
+    
+    input_text1 = pygame.Rect(50, 100, 32, 32)
+    input_left1 = pygame.Rect(50, 130, 16, 16)
+    input_right1 = pygame.Rect(66, 130, 16, 16)
+
+    input_text2 = pygame.Rect(100, 100, 32, 32)
+    input_left2 = pygame.Rect(100, 130, 16, 16)
+    input_right2 = pygame.Rect(116, 130, 16, 16)
+
+    result1_texte = pygame.Rect(150, 100, 140, 40)
+
+    buttonAStar = pygame.Rect(50, 150, 140, 40)
+
+    buttonDijkstra = pygame.Rect(200, 150, 140, 40)
+
+    box = pygame.Rect(30, 30, 450, 200)
+    box_border = pygame.Rect(30, 30, 450, 200)
+
+    text1 = font_title.render("La distance optimale entre: ", True, noir)
+    texte_input1 = "5"
+    texte_input2 = "4"
+    global noeudA
+    global noeudB
+    global isNotRun
+    global text_result
+    mx, my = pygame.mouse.get_pos()
+    if input_left1.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Réduit le numéro du Noeud A
+            noeudA = Less(noeudA)
+            isNotRun = False    
+    if input_right1.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Augmente le numéro du Noeud A
+            noeudA = More(noeudA, nb_noeud)
+            isNotRun = False 
+    if input_left2.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Réduit le numéro du Noeud B
+            noeudB =Less(noeudB)
+            isNotRun = False 
+    if input_right2.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Augmente le numéro du Noeud B
+            noeudB = More(noeudB, nb_noeud)
+            isNotRun = False 
+    if buttonDijkstra.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Calcule de la distance optimal
+            distance = CalDistanceDijkstra(noeudA, noeudB, positions_noeuds, position_liens)
+            if distance is not None:
+                text_result = f"Algo de Dijkstra: La distance est: {distance}."
+            else:
+                text_result = f"Il n'y a pas de chemin entre {noeudA} et {noeudB}."
+            isNotRun = False
+    if buttonAStar.collidepoint((mx, my)):
+        if click and isNotRun:
+            # Calcule de la distance optimal
+            distance = CalDistanceAStar(noeudA, noeudB, positions_noeuds, position_liens)
+            if distance is not None:
+                text_result = f"Algo de A*: La distance est: {distance}."
+            else:
+                text_result = f"Il n'y a pas de chemin entre {noeudA} et {noeudB}."
+            isNotRun = False 
+    
+    # Dessiner les éléments de l'interface
+    pygame.draw.rect(fenetre, (255, 255, 255), box, 0)
+    pygame.draw.rect(fenetre, noir, box_border, 2)
+    pygame.draw.rect(fenetre, noir, input_text1, 2)
+    pygame.draw.rect(fenetre, noir, input_text2, 2)
+    pygame.draw.rect(fenetre, noir, input_right1, 1)
+    pygame.draw.rect(fenetre, noir, input_right2, 1)
+    pygame.draw.rect(fenetre, noir, input_left1, 1)
+    pygame.draw.rect(fenetre, noir, input_left2, 1)
+    pygame.draw.rect(fenetre, noir, buttonAStar, 1)
+    pygame.draw.rect(fenetre, noir, buttonDijkstra, 1)
+    
+    fenetre.blit(text1, (50, 50))
+
+    # Afficher le texte des champs de texte
+    result1 = font_25.render(text_result, True, noir)
+    texte_surface1 = font_40.render(noeudA, True, noir)
+    texte_surface2 = font_40.render(noeudB, True, noir)
+    more = font_40.render("+", True, noir)
+    less = font_40.render("-", True, noir)
+    text_buttonAStar = font_40.render("AStar", True, noir)
+    text_buttonDijkstra = font_40.render("Dijkstra", True, noir)
+    fenetre.blit(texte_surface1, (input_text1.x + 6, input_text1.y+3))
+    fenetre.blit(texte_surface2, (input_text2.x + 6, input_text1.y+3))
+    fenetre.blit(text_buttonAStar, (buttonAStar.x + 6, buttonAStar.y+3))
+    fenetre.blit(text_buttonDijkstra, (buttonDijkstra.x + 6, buttonDijkstra.y+3))
+    fenetre.blit(more, (input_right1.x, input_right1.y-7))
+    fenetre.blit(more, (input_right2.x, input_right2.y-7))
+    fenetre.blit(less, (input_left1.x+3, input_left1.y-7))
+    fenetre.blit(less, (input_left2.x+3, input_left2.y-7))
+    fenetre.blit(result1, (result1_texte.x, result1_texte.y-7))
+
+def More(nb, nb_noeud):
+    if int(nb) == nb_noeud-1:
+        return nb
+    else:
+        return str(int(nb) + 1)
+
+def Less(nb):
+    if int(nb) == 0:
+        return str(0)
+    else:
+        return str(int(nb) - 1)
+
+def CalDistanceDijkstra(noeudA, noeudB, positions_noeuds, position_liens):
+    start_node = int(noeudA)
+    end_node = int(noeudB)
+
+    # Construire le graphique à partir des positions et liens
+    graph = {i: [] for i in range(len(positions_noeuds))}
+    for lien in position_liens:
+        graph[lien[0]].append((lien[1], 1))  # Poids des liens = 1
+        graph[lien[1]].append((lien[0], 1))
+
+    # Initialiser les distances à l'infini sauf pour le nœud de départ
+    distances = {node: float('infinity') for node in graph}
+    distances[start_node] = 0
+
+    # File de priorité pour maintenir les nœuds non explorés
+    priority_queue = [(0, start_node)]
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        # Si on atteint le nœud de destination, retourner la distance
+        if current_node == end_node:
+            return current_distance
+
+        # Parcourir les voisins du nœud actuel
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            # Mettre à jour la distance si un chemin plus court est trouvé
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    # Si on ne trouve pas de chemin, retourner None
+    return None
+
+def heuristic(node, end_node, positions_noeuds):
+    # Heuristique basée sur la distance euclidienne
+    x1, y1 = positions_noeuds[node]
+    x2, y2 = positions_noeuds[end_node]
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+def CalDistanceAStar(noeudA, noeudB, positions_noeuds, position_liens):
+    start_node = int(noeudA)
+    end_node = int(noeudB)
+
+    # Construire le graphique à partir des positions et liens
+    graph = {i: [] for i in range(len(positions_noeuds))}
+    for lien in position_liens:
+        graph[lien[0]].append((lien[1], 1))  # Poids des liens = 1
+        graph[lien[1]].append((lien[0], 1))
+
+    # File de priorité pour maintenir les nœuds non explorés
+    priority_queue = [(0 + heuristic(start_node, end_node, positions_noeuds), 0, start_node)]
+    visited = set()
+
+    while priority_queue:
+        current_total_cost, current_cost, current_node = heapq.heappop(priority_queue)
+
+        if current_node in visited:
+            continue
+
+        visited.add(current_node)
+
+        if current_node == end_node:
+            return current_cost
+
+        for neighbor, weight in graph[current_node]:
+            if neighbor not in visited:
+                priority = current_cost + weight + heuristic(neighbor, end_node, positions_noeuds)
+                heapq.heappush(priority_queue, (priority, current_cost + weight, neighbor))
+
+    # Si on ne trouve pas de chemin, retourner None
+    return None
 
 def GraphiqueReseau(largeur, hauteur, positions_noeuds, position_liens, rayon_noeud):
     # Initialisation de Pygame
     pygame.init()
+    
     # Couleurs
     blanc = (255, 255, 255)
     noir = (0, 0, 0)
@@ -79,22 +297,36 @@ def GraphiqueReseau(largeur, hauteur, positions_noeuds, position_liens, rayon_no
     dragging = False
     dragged_node = None
 
+    # Variable pour déclencher DrawOptionMenu
+    draw_option_menu = False
+
+    # Variable pour savoir si je click ou non
+    click = False
+
     # Boucle principale
     continuer = True
     while continuer:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 continuer = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Clic gauche
+                    click = True
                     for i, position in enumerate(positions_noeuds):
                         if math.dist(event.pos, position) <= rayon_noeud:
                             dragging = True
                             dragged_node = i
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
+                    global isNotRun 
+                    isNotRun= True
+                    click = False
                     dragging = False
                     dragged_node = None
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    draw_option_menu = not draw_option_menu  # Inverser l'état
 
         if dragging and dragged_node is not None:
             # Mettre à jour la position du nœud en fonction de la position de la souris
@@ -104,18 +336,17 @@ def GraphiqueReseau(largeur, hauteur, positions_noeuds, position_liens, rayon_no
         fenetre.fill(blanc)
 
         # Dessiner les liens
-        for position in position_liens:
-            pygame.draw.line(fenetre, noir, positions_noeuds[position[0]], positions_noeuds[position[1]], 2)
+        DrawLink(fenetre, noir, positions_noeuds, position_liens)
 
         # Dessiner les nœuds avec un numéro d'identification
-        for i, position in enumerate(positions_noeuds):
-            pygame.draw.circle(fenetre, noir, position, rayon_noeud)
-            # Afficher le numéro d'identification
-            font = pygame.font.Font(None, 36)
-            text = font.render(str(i), True, blanc)
-            text_rect = text.get_rect()
-            text_rect.center = position
-            fenetre.blit(text, text_rect)
+        DrawNoeud(positions_noeuds,fenetre, noir, blanc, rayon_noeud)
+
+        # Dessiner le diamètre du réseau
+        DrawDiametre(fenetre, positions_noeuds, position_liens, noir)
+
+        # Dessiner le menu d'options si la variable est True
+        if draw_option_menu:
+            DrawCalDistance(fenetre, noir, click, positions_noeuds, position_liens)
 
         pygame.display.flip()
 
@@ -158,8 +389,8 @@ def CalculerDiametre(positions_noeuds, position_liens):
     return diametre
 
 # Main
-largeur = 800
-hauteur = 600
+largeur = 1000
+hauteur = 800
 rayon_noeud = 20
 n_noeuds = random.randint(3, 30)
 if (n_noeuds == 3):
@@ -170,9 +401,5 @@ else:
     n_liens = random.randint((n_noeuds - 1), ((n_noeuds * 4) / 2))
 positions_noeuds = PositionsNoeuds(n_noeuds, largeur, hauteur, rayon_noeud)
 position_liens = PositionLiens(n_liens, n_noeuds)
-
-# calcul  du diamètre :
-diametre = CalculerDiametre(positions_noeuds, position_liens)
-print("Le diamètre du réseau est :", diametre)
 
 GraphiqueReseau(largeur, hauteur, positions_noeuds, position_liens, rayon_noeud)
